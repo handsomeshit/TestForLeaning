4edb92ecc155097c68b9240f74e16f8ac255e1b0
liangmingxing <1018451918@qq.com>
2024-03-27 16:56:55 +0800

    验证功能是否正常

--- MyTest/src/main/java/com/betaTest/BetaTestApplication.java
+++ MyTest/src/main/java/com/betaTest/BetaTestApplication.java
@@ -1,6 +1,9 @@
 package com.betaTest;
 
+import com.betaTest.domain.ModifyLogPageDTO;
 import com.betaTest.service.impl.EmailService;
+import com.betaTest.service.impl.ModifyLogServiceImpl;
+import com.github.pagehelper.PageInfo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,8 +23,22 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @MapperScan("com.betaTest.mapper")
 public class BetaTestApplication {
     public static void main(String[] args) {
+//        sendEmail(args);
+//        testModifyLog(args);
+    }
+
+    public static void sendEmail(String[] args) {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(BetaTestApplication.class, args);
         EmailService emailService = applicationContext.getBean(EmailService.class);
         emailService.sendEmail("2609360954@qq.com", "测试");
+    }
+
+    public static void testModifyLog(String[] args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(BetaTestApplication.class, args);
+        ModifyLogServiceImpl modifyLogService = applicationContext.getBean(ModifyLogServiceImpl.class);
+        ModifyLogPageDTO modifyLogPageDTO = new ModifyLogPageDTO();
+        modifyLogPageDTO.setUniqueFlag("48");
+        PageInfo pageInfo = modifyLogService.selectModifyLogWithDetailList(modifyLogPageDTO);
+        System.out.println(pageInfo);
     }
 }
